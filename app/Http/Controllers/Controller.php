a08df3b59c10555e7aab179b6ccb0991e8e80b11
Taras Kotya <taras.victorovich.96@gmail.com>
2022-02-04 21:34:06 +0200

    (from taras-home): --Taras-Kotya--

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -21,13 +21,43 @@ class Controller extends BaseController
         return view('index', $array);
     }
 
+
     public function add_user()
+    {
+        return view('add_user');
+    }
+
+
+    public function add_user_post()
     {
 
         DB::insert('insert into USERS (email,password) values (?, ?)', [$_POST['email'], $_POST['password']]);
 
         return redirect('/');
     }
+
+
+    public function deluser()
+    {
+        $users = DB::delete('delete from USERS where id=?', [$_GET['id']]);
+        return redirect('/');
+    }
+
+
+    public function edit($id)
+    {
+        $user = DB::select('select * from USERS where id=?',[$id]);
+        return view('add_user',['user' => $user[0]]);
+    }
+
+
+    public function save_user($id)
+    {
+        DB::update('update USERS set email = ?, password=? where id = ?', [$_POST['email'], $_POST['password'], $id]);
+
+        return redirect('/');
+    }
+
 
     public function pi()
     {
